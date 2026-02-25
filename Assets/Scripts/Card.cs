190155c8e9e2eda8747312db2a8ec42bdf442f02
supershem24 <super.sj24@gmail.com>
2026-02-25 17:52:02 -0500

    Created a base, but code isn't working, I need to start with getting my IDE set up first

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+
+    string color;
+    string name;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Flip!");
+        }*/
+    }
+
+    void Flip()
+    {
+        //gameObject.transform.rotation.y += -180;
+    }
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+
+    public Card[] cards;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ShuffleDeck()
+    {
+
+    }
+}
