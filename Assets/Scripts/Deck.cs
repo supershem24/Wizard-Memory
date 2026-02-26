002cfd5f1464b86fc8a5e3c7cb8e52b1fb88e3be
supershem24 <super.sj24@gmail.com>
2026-02-26 16:42:51 -0500

    Created and made a deck (UNTESTED)

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -1,14 +1,17 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Deck : MonoBehaviour
 {
 
-    public Card[] cards;
+    public List<Card> deckCards;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ShuffleDeck();
+        DealCard();
     }
 
     // Update is called once per frame
@@ -17,8 +20,32 @@ public class Deck : MonoBehaviour
         
     }
 
+    //UNTESTED
     void ShuffleDeck()
     {
+        List<Card> newCards = new List<Card>(deckCards.Count);
+        for (int i = 0;  i < deckCards.Count; i++)
+        {
+            int rand = Random.Range(0, deckCards.Count - 1);
+            if (newCards[rand] != null)
+            {
+                i--;
+                continue;
+            }
 
+            newCards[rand] = deckCards[i];
+        }
+        deckCards = newCards;
+    }
+
+    //UNTESTED
+    Card DealCard()
+    {
+        if (deckCards.Count <= 0)
+            Debug.LogError("NO MORE CARDS IN DECK"); return null;
+
+        Card card = deckCards[-1];
+        deckCards.RemoveAt(deckCards.Count - 1);
+        return card;
     }
 }
