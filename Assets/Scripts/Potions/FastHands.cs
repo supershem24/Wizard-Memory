ca8538e58d0d36752836bf7e24092b41ade66509
supershem24 <super.sj24@gmail.com>
2026-04-07 02:48:53 -0400

    Potion classes, yay

---
+++ Assets/Scripts/Potions/FastHands.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FastHands : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
